8835b1b148d6db785cf991531b6b5497493943ae
Niebelungen <elijah.westerberg@kearsarge.org>
2016-10-06 11:16:48 -0400

    added to story 1

--- story1.py
+++ story1.py
@@ -4,9 +4,14 @@ from input import *
 def story():
     location1 = getWord("Enter a location: ")
     temperature1 = getNumber("Enter a Number: ")
+    noun1 = getWord("Enter Noun: ")
+    noun2 = getWord("Enter Noun: ")
     
     text = ""
     text += "One day I went to the " + location1
     text += ". It was like a " + temperature1
-    text += " out."    
+    text += " out."
+    text += " I saw a " + noun1 
+    text += " walking towards " + noun2 
+    text += " . " 
     return text
