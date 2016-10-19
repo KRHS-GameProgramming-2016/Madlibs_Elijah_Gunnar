c9230da60add3058b0fc8274c4a0ba18e7867c07
Person21 <gunnar.andersson@kearsarge.org>
2016-10-19 11:56:09 -0400

    recent most this one is

--- story3.py
+++ story3.py
@@ -3,24 +3,11 @@ from input import *
 #Written by Gunnar and Elijah
 def story():
     location1 = getWord("Enter a location: ")
-    temperature1 = getWord("Enter a life decision: ")
     noun1 = getWord("Enter Name: ")
-    noun6 = getWord("Enter  Another Name: ")
-    noun2 = getWord("Enter Thing: ")
-    noun4 = getWord("Enter Another Thing: ")
-    noun3 = getWord("Enter A Creature: ")
-    noun5 = getWord("Enter A Color: ")
-    verb1 = getWord("Enter Verb, any will do: ")
-    verb2 = getWord("Enter Violent Action: ")
-    verb6 = getWord('Enter Violent Action that ends in "d": ')
-    verb3 = getWord("Enter Feeling: ")
-    verb4 = getWord('Enter Movement that ends in "-ing": ')
-    verb5 = getWord("Enter Interaction: ")
+    verb1 = getWord("Enter Action: ")
     adjective1 = getWord("Enter Object: ")
-    adjective2 = getWord("Enter Intensity: ")
-    adjective3 = getWord("Enter Higher Intensity: ")
-    whatever1 = getWord("Enter Random uniteligble screeching: ")
-    whatever2 = getWord("Enter Random uniteligble screeching, but even more uninteligble this time: ")
     
     text + ""
-    text += "
+    text += "Among the multitude of dead bodies, " + noun1 + " was slowly crawling their way out of the pit. "
+    text += "Their body covered in wounds, still, they managed to escape from " + location1 + ", where their allies lay dead and "
+    text += "rotting. Standing above the mass grave, " + noun1 + verb1
